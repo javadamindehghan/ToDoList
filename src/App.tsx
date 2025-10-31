945b07de56d0a225e58f5ad1ce8bb3d3a0be9a5f
javadamindehghan <javad.amindehghan1@gmail.com>
2025-10-31 12:44:47 -0700

    List

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import "./App.css";
-import { useAddList } from "./store/List";
+
 import Hedear from "./pages/Hedear";
 import Footer from "./pages/Footer";
 import Home from "./pages/Home";
-import { useEffect } from "react";
+
 
 const App: React.FC = () => {
-  const { count } = useAddList();
-
-  useEffect(() => {
-    localStorage.setItem("List", JSON.stringify(count));
-  }, [count]);
+ 
 
   return (
     <>
