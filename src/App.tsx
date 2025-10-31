cfb6e326852fc9e13d631a87dd7701537847300a
javadamindehghan <javad.amindehghan1@gmail.com>
2025-10-31 12:28:51 -0700

    ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Modal from "./components/Modal";
-import Form from "./components/Form";
 import { useAddList } from "./store/List";
-import ListTable from "./components/ListTable";
 import Hedear from "./pages/Hedear";
 import Footer from "./pages/Footer";
 import Home from "./pages/Home";
+import { useEffect } from "react";
 
 const App: React.FC = () => {
   const { count } = useAddList();
